28f3854afc9c12b420bacb1a8c90ceca4956d8fb
Georgie Horth <georgiehorth@gmail.com>
2021-02-16 14:55:37 +0000

    Created a simple web scraper which prints one random scraped wikipedia link

---
+++ scraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/Web_scraping",
+)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+title = soup.find(id="firstHeading")
+# print(title.content)
+
+# Get all the links
+allLinks = soup.find(id="bodyContent").find_all("a")
+random.shuffle(allLinks)
+linkToScrape = 0
+
+for link in allLinks:
+	# We are only interested in other wiki articles
+	if link['href'].find("/wiki/") == -1:
+		continue
+
+	# Use this link to scrape
+	linkToScrape = link
+	break
+
+print(linkToScrape)
