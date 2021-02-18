cb645715e13af693ec234aeeefe6e3a18f1008d9
Georgie Horth <georgiehorth@gmail.com>
2021-02-18 09:56:55 +0000

    Changed logic. Finds jobs from Monster.com and returns simple overview in CL

--- scraper.py
+++ scraper.py
@@ -1,27 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping",
-)
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Texas'
+page = requests.get(URL)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-title = soup.find(id="firstHeading")
-# print(title.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='ResultsContainer')
 
-# Get all the links
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linkToScrape = 0
+# searches for python only jobs
+python_jobs = results.find_all('h2', string=lambda text: 'python' in text.lower())
+for p_job in python_jobs:
+    link = p_job.find("a")["href"]
+    print(p_job.text.strip())
+    print(f"Apply here: {link}\n")
 
-for link in allLinks:
-	# We are only interested in other wiki articles
-	if link['href'].find("/wiki/") == -1:
-		continue
+# all job results from Texas
+job_elems = results.find_all('section', class_='card-content')
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
 
-	# Use this link to scrape
-	linkToScrape = link
-	break
+# print(results.prettify())
 
-print(linkToScrape)
+# UK monster frontend does not use ids.... 
+# URL = 'https://www.monster.co.uk/jobs/search?q=software+developer&where=London'
+# page = requests.get(URL)
+
+# soup = BeautifulSoup(page.content, 'html.parser')
+
+# results = soup.find_all('section', class='results-list split-screen-mode')
+# print(results)
